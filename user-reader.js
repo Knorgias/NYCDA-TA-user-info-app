becb9a83ce94513399f3403f0d582854ddebd6d9
Kostas Norgias <kostasnorgias@gmail.com>
2017-02-09 16:29:37 +0100

    added comments to user-reader.js

--- user-reader.js
+++ user-reader.js
@@ -3,11 +3,12 @@ const fs = require('fs');
 var users = JSON.parse(fs.readFileSync('users.json'));
 
 module.exports = {
-
+/**Returns all the users */
   getUsers: function() {
     return users;
   },
 
+/**Returns a list of users based on the query- matching first or last names */
   searchUsers: function(query) {
     var results = [];
 
@@ -19,6 +20,7 @@ module.exports = {
     return results;
   },
 
+/**Takes a new user object as input and adds it to the specified json file */
   addUser: function(userObject) {
     users.push(userObject);
     fs.writeFile('users.json', JSON.stringify(users), (error, data) => {
@@ -29,10 +31,12 @@ module.exports = {
   }
 };
 
+/**Method that returns all the user first names that include the given string */
 function searchFirstName(input, user) {
   return user.firstname.toLowerCase().includes(input.toLowerCase());
 }
 
+/**Method that returns all the user last names that include the given string */
 function searchLastName(input, user) {
   return user.lastname.toLowerCase().includes(input.toLowerCase());
 }
