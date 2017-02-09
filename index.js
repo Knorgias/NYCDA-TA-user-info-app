d27f76858c7632b36dd3fad4c905267a167df86b
Kostas Norgias <kostasnorgias@gmail.com>
2017-02-09 17:03:26 +0100

    more comments

--- index.js
+++ index.js
@@ -1,42 +1,49 @@
+/**Create an express application */
 const express = require('express'),
 			pug = require('pug'),
 			morgan = require('morgan'),
 			bodyParser = require('body-parser'),
 			fs = require('fs');
 
-const userRoutes = require('./routes/users'),
-			searchRoutes = require('./routes/search'),
-			addRoutes = require('./routes/add-user');
-
 const app = express();
 		userStore = require('./user-reader');
 
-app.use(express.static('public'));
+/**Create route objects from the modules */
+const 	userRoutes = require('./routes/users'),
+		searchRoutes = require('./routes/search'),
+		addRoutes = require('./routes/add-user');
 
+/**Set pug as the view engine */
 app.set('view engine', 'pug');
 
+/**Set to use the public folder */
 app.use(express.static('./public'));
 
+/**Set to use the morgan module */
 app.use(morgan('dev'));
 
+/**To be seen */
 app.use(bodyParser.urlencoded({ extended: false }));
 
+/**Use the created routes objects within our app */
 app.use('/users', userRoutes);
 
 app.use('/search', searchRoutes);
 
 app.use('/add-user', addRoutes);
 
+/**Get request in root with callback function */
 app.get('/', (request, response) => {
 	response.render('index', { users: userStore.getUsers() });
 });
 
+/**Get request in  /api/search/* with results given to res as json */
 app.get('/api/search/*', (req, res) => {
   var results = userStore.searchUsers(req.params[0]);
   res.json(results);
 });
 
-
+/**Define the port of our app and trigger a custom function */
 app.listen(3000, function() {
  console.log('Web server started on port 3000');
 });
--- routes/search.js
+++ routes/search.js
@@ -1,20 +1,23 @@
+/**Create an app express object and a router express.Router object */
 const express = require('express'),
       fs = require('fs'),
       router = express.Router(),
       app = express();
 
+/**Create userStore object from custom user-reader module */
 const userStore = require('./../user-reader');
 
-
+/**Define the render file for get requests from root */
 router.get('/', (request, response) => {
   response.render('search/search-users');
 });
 
+/**Define the redirect file for post requests from root */
 router.post('/', (request, reponse) => {
   reponse.redirect('/search/' + request.body.query);
 });
 
-
+/**Define the render file for get requests from root with a parameter :query*/
 router.get('/:query', (request, response) => {
   var results = userStore.searchUsers(request.params.query);
   console.log('RESULTS ARE:');
@@ -24,4 +27,5 @@ router.get('/:query', (request, response) => {
 
 });
 
+/**Export router object as a module */
 module.exports = router;
